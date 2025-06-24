5c23213760823928ad757c1a0703ada91d0cba8a
Tribhuvan nath <tribhuvannath4567@gmail.com>
2025-06-24 18:52:29 +0530

    first commit

---
+++ MovieDekho/src/main/java/com/movieDekho/MovieDekho/MovieDekhoApplication.java
@@ -0,0 +1,13 @@
+package com.movieDekho.MovieDekho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDekhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieDekhoApplication.class, args);
+	}
+
+}
---
+++ MovieDekho/src/test/java/com/movieDekho/MovieDekho/MovieDekhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieDekho.MovieDekho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDekhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
