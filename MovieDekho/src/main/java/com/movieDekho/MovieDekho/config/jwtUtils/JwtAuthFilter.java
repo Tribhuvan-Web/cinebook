d820920465104f9dcea7c6014be1ea1d8c262397
Tribhuvan nath <tribhuvannath4567@gmail.com>
2025-08-16 23:32:38 +0530

    implemented race condition

--- MovieDekho/src/main/java/com/movieDekho/MovieDekho/config/jwtUtils/JwtAuthFilter.java
+++ MovieDekho/src/main/java/com/movieDekho/MovieDekho/config/jwtUtils/JwtAuthFilter.java
@@ -4,8 +4,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,8 +17,6 @@ import java.io.IOException;
 
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
-    
-    private static final Logger logger = LoggerFactory.getLogger(JwtAuthFilter.class);
     
     @Autowired
     private JwtUtils jwtUtils;
